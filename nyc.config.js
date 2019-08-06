0929f8376aff1ca6ca2128dd7b402384bae57f22
Kosaku Kimura <kimusaku@gmail.com>
2019-08-07 02:17:57 +0900

    modify reporter

--- nyc.config.js
+++ nyc.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  reporter: ['text', 'lcov'],
+  reporter: ['text', 'test-lcov'],
   'temp-directory': './coverage/.nyc_output',
   all: true,
   sourceMap: true,

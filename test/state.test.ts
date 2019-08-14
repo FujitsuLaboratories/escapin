f3104eea4f23f01d7bf75a38d25a8c63d628a91f
Kosaku Kimura <kimusaku@gmail.com>
2019-08-14 22:58:37 +0900

    add test cases

--- test/state.test.ts
+++ test/state.test.ts
@@ -1,3 +1,58 @@
 import test from 'ava';
+import path from 'path';
+import * as u from '../src/util';
+import { BaseState } from '../src/state';
+import { initializeState } from './util';
 
-test.todo('test state');
+test('test getPathInfo', t => {
+  t.is(new BaseState().getPathInfo('incomplete'), undefined);
+
+  const state = initializeState();
+  t.deepEqual(state.getPathInfo('csvGET'),  {
+    name: 'test-test',
+    path: '/csv',
+    method: 'get',
+    consumes: ['application/json'],
+    produces: [],
+    parameters: [],
+  });
+
+  t.is(state.getPathInfo('invalid'), undefined);
+});
+
+test('test pushProgramBody', t => {
+  const state = initializeState();
+  state.ast = u.parse('');
+  state.pushProgramBody(u.parse('hoge();').program.body[0]);
+  t.deepEqual(u.purify(state.ast), u.purify(u.parse("hoge();")));
+
+  state.pushProgramBody(u.parse("piyo(); fuga();").program.body);
+  t.deepEqual(u.purify(state.ast), u.purify(u.parse("hoge(); piyo(); fuga();")));
+});
+
+test('test unshiftProgramBody', t => {
+  const state = initializeState();
+  state.ast = u.parse('');
+  state.unshiftProgramBody(u.parse('hoge();').program.body[0]);
+  t.deepEqual(u.purify(state.ast), u.purify(u.parse("hoge();")));
+
+  state.unshiftProgramBody(u.parse("piyo(); fuga();").program.body);
+  t.deepEqual(u.purify(state.ast), u.purify(u.parse("piyo(); fuga(); hoge();")));
+});
+
+test('test resolvePath', t => {
+  const state = initializeState();
+  t.is(state.resolvePath('src/index.ts'), path.resolve('src/index.ts'));
+  t.is(state.resolvePath('ava.config'), path.resolve('ava.config.js'));
+  t.is(state.resolvePath('invalid'), undefined);
+});
+
+test('test hasDependency', t => {
+  t.is(new BaseState().hasDependency('hoge'), false);
+
+  const state = initializeState();
+  state.escapin.packageJson.dependencies['hoge'] = 'latest';
+  t.is(state.hasDependency('fs'), true);
+  t.is(state.hasDependency('hoge'), true);
+  t.is(state.hasDependency('piyo'), false);
+});
--- test/util.ts
+++ test/util.ts
@@ -5,20 +5,16 @@ import path from 'path';
 import { sync as rimraf } from 'rimraf';
 import { promisify } from 'util';
 import { v4 as uuid } from 'uuid';
-import * as c from '../src/util';
+import * as u from '../src/util';
 import { BaseState } from '../src/state';
 import { Escapin } from '../src';
 import * as types from '../src/types';
 
 const mkdirp = promisify(_mkdirp);
 
-export async function compare(
-  t: ExecutionContext,
-  testName: string,
-  ...steps: Array<(state: BaseState) => void>
-) {
+export function initializeState() {
   const state = new BaseState();
-  state.filename = 'test';
+  state.filename = 'test.js';
   state.escapin = new Escapin(process.cwd());
   state.escapin.id = 'test';
   state.escapin.config = {
@@ -39,6 +35,7 @@ export async function compare(
         '/csv': {
           get: {
             'x-escapin-handler': 'test.csvGET',
+            consumes: ['application/json'],
           },
         },
       },
@@ -64,14 +61,22 @@ export async function compare(
   state.escapin.types.put(types.errorFirstCallback('errorFirstCallbackFunc'));
   state.escapin.types.put(types.generalCallback('generalCallbackFunc'));
   state.escapin.types.put(types.general('generalFunc'));
+  return state;
+}
 
+export async function compare(
+  t: ExecutionContext,
+  testName: string,
+  ...steps: Array<(state: BaseState) => void>
+) {
+  const state = initializeState();
   await mkdirp(state.escapin.config.output_dir);
 
   const before = fs.readFileSync(path.join(__dirname, `steps/${testName}.in.js`), 'utf8');
   const after = fs.readFileSync(path.join(__dirname, `steps/${testName}.out.js`), 'utf8');
 
   try {
-    const astBefore = c.parse(before);
+    const astBefore = u.parse(before);
     state.code = before;
     state.ast = astBefore;
 
@@ -79,9 +84,9 @@ export async function compare(
       step(state);
     }
 
-    const astAfter = c.parse(after);
+    const astAfter = u.parse(after);
 
-    t.deepEqual(c.purify(astBefore), c.purify(astAfter));
+    t.deepEqual(u.purify(astBefore), u.purify(astAfter));
   } catch (err) {
     console.error(err);
     t.fail();

bfad16e406f7c70a7d30a412b255ee6a7909d782
Kosaku Kimura <kimusaku@gmail.com>
2019-10-23 16:07:37 +0900

    fix usage of DynamoDB scan

--- templates/snippet/aws/table/keys.js
+++ templates/snippet/aws/table/keys.js
@@ -1,14 +1,16 @@
 const $VAR = [];
 while (true) {
-  const $TEMPVAR = new DynamoDB().scan({
+  const params = {
     TableName: $NAME,
     ExpressionAttributeNames: {
       '#ky': 'key',
     },
     ProjectionExpression: '#ky',
-  });
+  };
+  const $TEMPVAR = new DynamoDB().scan(params);
   $VAR.push(...$TEMPVAR.Items.map(item => item.key.S));
-  if ($TEMPVAR.LastEvaluatedKey !== undefined) {
+  if ($TEMPVAR.LastEvaluatedKey === undefined) {
     break;
   }
+  params.ExclusiveStartKey = $TEMPVAR.LastEvaluatedKey;
 }

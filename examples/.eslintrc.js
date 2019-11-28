95ca7c3c1ce23ccaf698d29c05344af8df2b5c92
Kosaku Kimura <kimusaku@gmail.com>
2019-11-28 12:17:57 +0900

    set config file formats from js to json

--- examples/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:node/recommended'],
-  rules: {
-    'no-unused-vars': 'off',
-    'no-undef': 'off',
-    'node/no-missing-import': 'off',
-    'node/no-unpublished-import': 'off',
-  },
-};

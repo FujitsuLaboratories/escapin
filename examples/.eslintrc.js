62899d0c2ba44f5add3c287c6fc506015d29fd6d
Kosaku Kimura <kimusaku@gmail.com>
2020-02-17 17:08:18 +0900

    chore: update eslint rules

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -7,10 +7,13 @@ module.exports = {
   rules: {
     'no-unused-vars': 'off',
     'no-undef': 'off',
+    'prefer-const': 'off',
     'node/no-missing-import': 'off',
     'node/no-missing-require': 'off',
     'node/no-unpublished-import': 'off',
     'node/no-unpublished-require': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
--- templates/snippet/.eslintrc.js
+++ templates/snippet/.eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
   parserOptions: { ecmaVersion: 6, sourceType: 'module' },
   env: {
     es6: true,
   },
   rules: {
     'no-constant-condition': 'off',
-    'no-unused-vars': 'off',
     'no-undef': 'off',
+    'prefer-const': 'off',
     'node/no-extraneous-import': 'off',
     'node/no-missing-import': 'off',
     'node/no-unpublished-import': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
     'prettier/prettier': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };

d1df1099ca79b283f4bdd31c15e60850e0bb7d40
Kosaku Kimura <kimusaku@gmail.com>
2020-04-13 10:42:43 +0900

    fix(*): update commit-analyzer config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,21 @@ module.exports = {
     branches: ['master'],
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'refactor', scope: 'core-*', release: 'minor' },
+          { type: 'refactor', release: 'patch' },
+          { scope: 'no-release', release: false },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     [

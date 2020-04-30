1aea3e54a1428d46b957b84ae7ef67c592addf24
Kosaku Kimura <kimusaku@gmail.com>
2020-04-30 14:48:38 +0900

    chore(*): move examples to other repo

--- examples/sendmail/.escapinrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'sendmail',
-  api_spec: 'swagger.yaml',
-  credentials: [{ api: 'mailgun API', basicAuth: 'api:<YOUR_API_KEY>' }],
-  platform: 'aws',
-  output_dir: 'build',
-};

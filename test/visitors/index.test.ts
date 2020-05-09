5f7abae8e86c0f44fdcc4e3e0d325e90e76cee43
Kosaku Kimura <kimusaku@gmail.com>
2020-05-09 22:17:32 +0900

    fix(testing): use mocha, chai and nyc instead of jest

--- test/visitors/index.test.ts
+++ test/visitors/index.test.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import {
   normalize,
   uncallbackify,
@@ -10,80 +11,87 @@ import {
 } from '../../src/visitors';
 import { transpile } from '../util';
 
-test('[normalize.1] nominal case of normalize', () => {
-  const { actual, expected } = transpile('normalize.1', {}, normalize);
-  expect(actual).toEqual(expected);
-});
+describe('visitors', function () {
+  this.timeout(0);
 
-test('[uncallbackify.1] nominal case of uncallbackify', () => {
-  const { actual, expected } = transpile('uncallbackify.1', {}, uncallbackify);
-  expect(actual).toEqual(expected);
-});
+  it('[normalize.1] nominal case of normalize', () => {
+    const { actual, expected } = transpile('normalize.1', {}, normalize);
+    expect(actual).to.eql(expected);
+  });
 
-test('[openApiV2.1] ignore conventional import declarations', () => {
-  const { actual, expected } = transpile(
-    'openApiV2.1',
-    {},
-    openApiV2,
-    finalize,
-  );
-  expect(actual).toEqual(expected);
-});
+  it('[uncallbackify.1] nominal case of uncallbackify', () => {
+    const { actual, expected } = transpile(
+      'uncallbackify.1',
+      {},
+      uncallbackify,
+    );
+    expect(actual).to.eql(expected);
+  });
 
-test('[openApiV2.2] nominal case of openApiV2 with axios', done => {
-  const { actual, expected } = transpile(
-    'openApiV2.2',
-    {},
-    openApiV2,
-    finalize,
-  );
-  expect(actual).toEqual(expected);
-  done();
-});
+  it('[openApiV2.1] ignore conventional import declarations', () => {
+    const { actual, expected } = transpile(
+      'openApiV2.1',
+      {},
+      openApiV2,
+      finalize,
+    );
+    expect(actual).to.eql(expected);
+  });
 
-test('[openApiV2.3] nominal case of openApiV2 with request', () => {
-  const { actual, expected } = transpile(
-    'openApiV2.3',
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    { http_client: 'request' },
-    openApiV2,
-    finalize,
-  );
-  expect(actual).toEqual(expected);
-});
+  it('[openApiV2.2] nominal case of openApiV2 with axios', () => {
+    const { actual, expected } = transpile(
+      'openApiV2.2',
+      {},
+      openApiV2,
+      finalize,
+    );
+    expect(actual).to.eql(expected);
+  });
 
-test('[refineObject.1] nominal case of refineObject', () => {
-  const { actual, expected } = transpile(
-    'refineObject.1',
-    {},
-    refineObject,
-    finalize,
-  );
-  expect(actual).toEqual(expected);
-});
+  it('[openApiV2.3] nominal case of openApiV2 with request', () => {
+    const { actual, expected } = transpile(
+      'openApiV2.3',
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      { http_client: 'request' },
+      openApiV2,
+      finalize,
+    );
+    expect(actual).to.eql(expected);
+  });
 
-test('[refineFunction.1] nominal case of refineFunction', () => {
-  const { actual, expected } = transpile(
-    'refineFunction.1',
-    {},
-    refineFunction,
-    finalize,
-  );
-  expect(actual).toEqual(expected);
-});
+  it('[refineObject.1] nominal case of refineObject', () => {
+    const { actual, expected } = transpile(
+      'refineObject.1',
+      {},
+      refineObject,
+      finalize,
+    );
+    expect(actual).to.eql(expected);
+  });
 
-test('[asynchronize.1] nominal case of asynchronize', () => {
-  const { actual, expected } = transpile(
-    'asynchronize.1',
-    {},
-    functionTypes,
-    asynchronize,
-    finalize,
-  );
-  expect(actual).toEqual(expected);
-});
+  it('[refineFunction.1] nominal case of refineFunction', () => {
+    const { actual, expected } = transpile(
+      'refineFunction.1',
+      {},
+      refineFunction,
+      finalize,
+    );
+    expect(actual).to.eql(expected);
+  });
 
-test('[finalize.1] nominal case of finalize', () => {
-  const { actual, expected } = transpile('finalize.1', {}, finalize);
-  expect(actual).toEqual(expected);
+  it('[asynchronize.1] nominal case of asynchronize', () => {
+    const { actual, expected } = transpile(
+      'asynchronize.1',
+      {},
+      functionTypes,
+      asynchronize,
+      finalize,
+    );
+    expect(actual).to.eql(expected);
+  });
+
+  it('[finalize.1] nominal case of finalize', () => {
+    const { actual, expected } = transpile('finalize.1', {}, finalize);
+    expect(actual).to.eql(expected);
+  });
 });

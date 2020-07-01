90b6b487e0d95be1f083e5e0c1375a2ea510e584
Kosaku Kimura <kimusaku@gmail.com>
2020-07-01 10:42:29 +0900

    feat(visitors): improve type checking for general-callback functions

--- test/visitors/asynchronize.1.out.js
+++ test/visitors/asynchronize.1.out.js
@@ -35,21 +35,57 @@ async function func() {
   }
 
   await Promise.all(_promise);
-}
-
-generalCallbackFunc(7, arg => {
-  const data = (() => {
+  generalCallbackFunc(7, () => {
     let _temp;
 
     let _done = false;
-    asyncFunc(8, arg).then(_data3 => {
+    (async arg => {
+      const _data2 = await asyncFunc(8, arg);
+
+      generalFunc(9);
+    })().then(_data3 => {
       _temp = _data3;
       _done = true;
     });
     deasync.loopWhile(_ => !_done);
     return _temp;
-  })();
+  });
+  generalFunc(10);
+  await new Promise((resolve, reject) => {
+    errorFirstCallbackFunc(11, (err, foo, bar) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({
+          foo,
+          bar,
+        });
+      }
+    });
+  });
+  foo.generalFunc(12);
+  const arr = [1, 2, 3];
+  await Promise.all(arr.map(async i => await asyncFunc(i, 13)));
+  arr.forEach(async i => await asyncFunc(i, 14));
+  generalCallbackFunc(15, () => {
+    let _temp2;
 
-  generalFunc(9);
-});
-generalFunc(10);
+    let _done2 = false;
+    (async arg => {
+      await new Promise((resolve, reject) => {
+        errorFirstCallbackFunc(16, (err, _data2) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(_data2);
+          }
+        });
+      });
+    })().then(_data6 => {
+      _temp2 = _data6;
+      _done2 = true;
+    });
+    deasync.loopWhile(_ => !_done2);
+    return _temp2;
+  });
+}
